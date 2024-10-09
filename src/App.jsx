92acd6d7236aa5a96ae76a0dca089adaaf07d3d7
Ergis Mullai <ergis@motomtech.com>
2024-10-09 09:07:27 +0200

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,16 @@
 import React, { useRef } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
-import { Box, OrbitControls } from "@react-three/drei";
-
-const Scene = () => {
-  const boxRef = useRef();
-  useFrame((state, delta) => {
-    boxRef.current.rotation.y += 0.02;
-  });
-
-  return (
-    <>
-      <Box ref={boxRef} args={[1, 1, 1]} rotation={[0.5, 0, 0]}>
-        <meshNormalMaterial />
-      </Box>
-      <ambientLight />
-    </>
-  );
-};
+import { SoftShadows, Float, CameraControls, Sky, PerformanceMonitor } from "@react-three/drei";
+import { Model } from "./Model";
 
 const App = () => {
   return (
-    <Canvas camera={{ fov: 70, position: [0, 0, 3] }}>
-      <OrbitControls />
-      <Scene />
+    <Canvas shadows camera={{ position: [5, 2, 10], fov: 50 }}>
+      <CameraControls makeDefault />
+      <color attach='background' args={["#d0d0d0"]} />
+      <ambientLight intensity={0.4} />
+      <Model scale={0.05} position={[0, -1, 0]} />
+      <Sky inclination={0.52} scale={20} />
     </Canvas>
   );
 };
